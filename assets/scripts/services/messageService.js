819d7c2c5ef86f69d3c5e6fc30bce8c624791ab4
Dylan Brown <dylandoesprogramming@gmail.com>
2017-08-01 15:14:11 -0500

    Finished project

--- assets/scripts/services/messageService.js
+++ assets/scripts/services/messageService.js
@@ -1,14 +1,18 @@
 'use strict';
 
 angular.module('chattyApp')
-  .service('messageService', function ( $http ) {
-    
-      this.getMessages = function () {
-        return $http.get('/messages');
-      }
+    .service('messageService', function($http) {
 
-      this.addMessage = function ( message ) {
-        return $http.post('/messages', { message: message });
-      }
-    
-  });
+        this.getMessages = function() {
+            return $http.get('/messages');
+        }
+
+        this.addMessage = function(message, username) {
+            console.log(message, username)
+            return $http.post('/messages', {
+                message: message,
+                username: username
+            });
+        }
+
+    });
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.static('assets'));
+app.use(bodyParser.json());
 
+let messages = [];
+
+
+app.get('/messages', function(req, res, next) {
+    res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function(req, res, next) {
+    console.log('trying to post')
+    messages.push({ message: req.body.message, time: new Date(), username: req.body.username });
+    $filter('date')
+    res.status(200).json({ messages: messages })
+})
+
+app.listen(3000, function() {
+    console.log('port 3000')
+})
